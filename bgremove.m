d4ee88bbff78663c22fbc52ea51655edf15f0838
Ruaridh Thomson <echelous@me.com>
2012-02-29 23:29:08 +0000

    bgRemove

---
+++ bgremove.m
@@ -0,0 +1,27 @@
+function binimage = bgremove (partim, bgim)
+
+    T = double(min(double(min(min(partim))+(max(max(partim))-min(min(partim)))/2))/255);
+
+    %detect change in red, green and blue values. Makes WxHx3 image
+    [H,W] = size(partim);
+    outim = zeros(H,W);
+    for r = 1 : H
+    for c = 1 : W
+      if abs(double(bgim(r,c)) - double(partim(r,c))) > T*50
+          outim(r,c) = double(1);
+      end
+    end
+    end
+
+
+    %combine red, green and blue elements detected to make one black and white image
+    [H,W] = size(outim);
+    outim2 = zeros(H,W/3);
+    for r = 1 : H
+    for c = 1 : W/3;
+      outim2(r,c) = double(outim(r,c)+outim(r,c+W/3)+outim(r,c+W*2/3));
+    end
+    end
+
+    binimage = outim2;
+
